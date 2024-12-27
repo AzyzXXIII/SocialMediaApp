2bcc7ec5e8f017669e016bfb483f5d41d9fa24e9
AzyzXXIII <azizhcini001@gmail.com>
2024-12-27 02:02:48 +0100

    creating tables

---
+++ models/posts.model.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  name: String,
+  media: [],
+  description: String,
+  creator: { type: mongoose.Types.ObjectId, ref: "users", required: true },
+});
+
+module.exports = mongoose.model("posts", postSchema);
