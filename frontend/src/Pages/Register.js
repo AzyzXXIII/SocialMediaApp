8350bb26bfa1ed5246e46bcbab8a5445006639a4
AzyzXXIII <azizhcini001@gmail.com>
2025-01-18 12:11:53 +0100

    link form field with variables

--- frontend/src/Pages/Register.js
+++ frontend/src/Pages/Register.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [bio, setBio] = useState("");
+  const [picture, setPicture] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+
   return (
     <div className="register">
       <div className="register-cover"></div>
@@ -9,34 +17,62 @@ const Register = () => {
         <p>social Media Application</p>
         <div>
           <from>
-            <div>
+            <div className="form-group">
               <label>FirstName</label>
-              <input type="text" name="firstName" required />
+              <input
+                type="text"
+                name="firstName"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
             </div>
-            <div>
+            <div className="form-group">
               <label>lastName</label>
-              <input type="text" name="lastName" required />
+              <input
+                type="text"
+                name="lastName"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
             </div>
-            <div>
+            <div className="form-group">
               <label>Email</label>
-              <input type="text" name="email" required />
+              <input
+                type="text"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
-            <div>
+            <div className="form-group">
               <label>¨Password</label>
-              <input type="password" required />
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
-            <div>
+            <div className="form-group">
               <label>Bio</label>
-              <textarea rows="2" required />
+              <textarea
+                rows="2"
+                value={bio}
+                onChange={(e) => setBio(e.target.value)}
+              />
             </div>
-            <div>
+            <div className="form-group">
               <label>Picture</label>
               <input type="file" />
             </div>
-            <div>
+            <div className="form-group">
               <label>BirthDate</label>
-              <input type="date" />
+              <input
+                type="date"
+                value={birthDate}
+                onChange={(e) => setBirthDate(e.target.value)}
+              />
             </div>
+            <button btn-form>Sign Up</button>
           </from>
         </div>
       </div>
