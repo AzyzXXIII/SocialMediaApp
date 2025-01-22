cf2d3f49582cfe77cc62bedc81b9968e90b54f48
AzyzXXIII <azizhcini001@gmail.com>
2025-01-22 14:40:39 +0100

    Retrieve connected user DATA

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const Home = () => {
+  const [connUserData, setConnUserData] = useState({});
+
+  const getConnectedUserData = () => {
+    setConnUserData(JSON.parse(localStorage.getItem("userData")));
+  };
+
+  useEffect(() => {
+    getConnectedUserData();
+  }, []);
   return (
     <div>
-      <h1>Heeey , welcome to Home page</h1>
+      <h1>
+        Heeey {connUserData.firstName + ` ` + connUserData.lastName}, welcome to
+        Home page
+      </h1>
     </div>
   );
 };
